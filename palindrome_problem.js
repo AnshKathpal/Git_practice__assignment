a758d12c462e4478b0f02da6364f41f1c277e253
Ravi Pal <ravistarpal@gmail.com>
2022-12-04 14:36:47 +0530

    check palindrome

--- palindrome_problem.js
+++ palindrome_problem.js
@@ -2,11 +2,18 @@
 
 let str = "madam";
 let bag = "";
-for(x=str.length-1;x>=1;x--){
+
+for(let x=str.length-1; x>=0; x--){
+  
 bag+=str[x];
+
 }
+
 if(bag == str){
 console.log("palindrome");
-}else {
-console.log("not palindrome);
+
+}
+else
+{
+console.log("not palindrome");
 }
